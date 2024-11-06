44f75cb510d5e09bff77ba9d7ba9946240323def
Yasmin Lino <yasmincriss00@gmail.com>
2024-11-06 15:35:48 -0300

    :arrow_up: Dependency update

---
+++ CleanArchMvc.Infra.Ioc/DependencyInjection.cs
@@ -0,0 +1,38 @@
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Repositories;
+using CleanArchMvc.Infra.Data.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Services;
+
+namespace CleanArchMvc.Infra.Ioc;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Configuração do MongoDB
+        services.Configure<MongoDbSettings>(configuration.GetSection("MongoDbSettings"));
+
+        // Registro do cliente MongoDB
+        services.AddSingleton<IMongoClient>(serviceProvider =>
+        {
+            var settings = serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+            return new MongoClient(settings.ConnectionString);
+        });
+
+        // Registre seus repositórios
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        return services;
+    }
+}
