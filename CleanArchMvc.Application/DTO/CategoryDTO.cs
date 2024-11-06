ffe5cb51ef0a4bfb95282705b4aee6f13581f73f
Yasmin Lino <yasmincriss00@gmail.com>
2024-11-06 11:26:33 -0300

    :sparkles: Creating DTOs and mappings with AutoMapper

---
+++ CleanArchMvc.Application/DTO/CategoryDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.Application.DTO;
+
+public class CategoryDTO
+{
+    public int Id { get; set;}
+
+    [Required(ErrorMessage = "The Name is Required")] // Obrigatório
+    [MinLength(3)] //Minimo de caracters
+    [MaxLength(100)] //Maximo de caracters
+    [DisplayName("Name")]    
+    public string? Name { get; private set; }
+}
---
+++ CleanArchMvc.Application/DTO/ProductDTO.cs
@@ -0,0 +1,45 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Application.DTO;
+
+public class ProductDTO
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "The Name is Required")] // Obrigatório
+    [MinLength(3)] //Minimo de caracters
+    [MaxLength(100)] //Maximo de caracters
+    [DisplayName("Name")]  
+    public string? Name { get; private set; }
+
+    [Required(ErrorMessage = "The Description is Required")] // Obrigatório
+    [MinLength(5)] //Minimo de caracters
+    [MaxLength(200)] //Maximo de caracters
+    [DisplayName("Description")]  
+    public string? Description { get; private set; }
+
+    [Required(ErrorMessage = "The Price is Required")] // Obrigatório
+    [Column(TypeName = "decimal(18,2)")]
+    [DisplayFormat(DataFormatString = "0:C2")]
+    [DataType(DataType.Currency)]
+    [DisplayName("Price")] 
+    public double Price { get; private set; }
+
+    [Required(ErrorMessage = "The Stock is Required")] // Obrigatório
+    [Range(1, 9999)]
+    [DisplayName("Stock")]
+    public int Stock { get; private set; }
+
+    [MaxLength(250)]
+    [DisplayName("Image")]
+    public string? Image { get; private set; }
+
+    public Category? Category { get; set; } 
+
+    [DisplayName("Categories")]
+    public int CategoryId { get; set; } 
+}
